3df45f6533155d0f1c19185d0005d934ac2e5944
arielrado <arielrado@gmail.com>
2023-06-09 04:49:11 +0300

    committed

--- serial-read.py
+++ serial-read.py
@@ -1,20 +1,23 @@
 import serial
 import json
 
-ser = serial.Serial('COM1')
-j = open('magnetic_data.json','wb')
+ser = serial.Serial('/dev/tty.usbserial-0001')
+j = open('magnetic_data_parking2.json','w')
 
 data = {}
-data['filed'] = []
+data['field'] = []
 data['distance'] = []
 
 try:
     while True:
         line = str(ser.readline())
-        data['distance'] += float(line.split(',')[0])
-        data['field'] += int(line.split(',')[1])
+        print(line)
+        line = line[2:-5]
+        data['distance'] += [float(line.split(',')[0])]
+        data['field'] += [int(line.split(',')[1])]
 except KeyboardInterrupt:
     pass
 
 json.dump(data, j)
 j.close()
+ser.close()
